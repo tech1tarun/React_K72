59106839e0d56c4baae494e6000190456feab864
tech1tarun <tarunkumarsingh248@gmail.com>
2026-02-04 23:11:39 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Agence from "./pages/Agence";
 import Projects from "./pages/Project";
+import NavBar from "./components/Navigation/NavBar";
 
 const App = () => {
   return (
     <div className="text-white">
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/agence" element={<Agence />} />
